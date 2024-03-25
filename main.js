6ba240c7eec2d07d62dc2f8944ef473c79d3fffb
Ammad Amin <159122493+hammadamin9999@users.noreply.github.com>
2024-03-26 02:10:56 +0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,30 @@
+#! /usr/bin/env node
+// BMI CALCULATOR USING INQUIRER REMOTE
+import inquirer from "inquirer";
+const questions = [
+    { type: 'number', name: 'weight', message: 'weight(kg):' },
+    { type: 'number', name: 'height', message: 'height(m):' },
+];
+const calculateBMI = (weight, height) => weight / (height * height);
+const getBMICategory = (bmi) => {
+    if (bmi < 18.5) {
+        return 'Underweight';
+    }
+    else if (bmi >= 18.5 && bmi < 26) {
+        return 'Normal weight';
+    }
+    else if (bmi >= 26 && bmi < 30) {
+        return 'Overweight';
+    }
+    else {
+        return 'Obese';
+    }
+};
+const main = async () => {
+    const { weight, height } = await inquirer.prompt(questions);
+    const bmi = calculateBMI(weight, height);
+    const bmiFormatted = bmi.toFixed(2);
+    const bmiCategory = getBMICategory(bmi);
+    console.log(`BMI: ${bmiFormatted} (${bmiCategory})`);
+};
+main();
